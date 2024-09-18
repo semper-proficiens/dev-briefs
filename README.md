90af54822b740b5bb3bc51bd2442193c2b9748aa
nubesFilius <eperezlarosa@outlook.com>
2024-09-18 00:58:25 -0400

    working_template_gui

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dev-briefs
 Central repo for dev-briefs
+
+# Tools
+
+- Uses air for quick page refresh:
+    - ```bash
+      go install github.com/air-verse/air@latest
+      ```
+    - run `air` in terminal for live refresh
--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	"html/template"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	customErrorMessage = "Oops. Something went wrong on our side."
 )
 
 type NewsItem struct {
@@ -20,8 +27,11 @@ type NewsItem struct {
 }
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/content", contentHandler)
+	http.HandleFunc("/news", newsHandler)
 	log.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
@@ -31,33 +41,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func contentHandler(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("http://localhost:8081/api/everything-hacking-news")
-	if err != nil {
-		http.Error(w, "Failed to fetch news", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	// if it's a htmx request
+	if strings.Contains(r.Header.Get("HX-Request"), "true") {
+		resp, err := http.Get("http://localhost:8081/api/everything-hacking-news")
+		if err != nil {
+			http.Error(w, customErrorMessage, http.StatusInternalServerError)
+			return
+		}
+		defer func() {
+			if err = resp.Body.Close(); err != nil {
+				slog.Error("failed to close client connection", "newsHandler", err)
+			}
+		}()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Failed to read response body", http.StatusInternalServerError)
-		return
-	}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "failed to read response body", http.StatusInternalServerError)
+			return
+		}
 
-	var newsMap map[string]NewsItem
-	err = json.Unmarshal(body, &newsMap)
-	if err != nil {
-		http.Error(w, "Failed to unmarshal JSON", http.StatusInternalServerError)
-		return
-	}
+		var newsMap map[string]NewsItem
+		err = json.Unmarshal(body, &newsMap)
+		if err != nil {
+			http.Error(w, "failed to unmarshal JSON", http.StatusInternalServerError)
+			return
+		}
 
-	// Convert map to slice for easier template rendering
-	var news []NewsItem
-	for _, item := range newsMap {
-		news = append(news, item)
-	}
+		// Convert map to slice for easier template rendering
+		var news []NewsItem
+		for _, item := range newsMap {
+			news = append(news, item)
+		}
 
-	tmpl := template.Must(template.ParseFiles("templates/content.html"))
-	tmpl.Execute(w, news)
+		tmpl := template.Must(template.ParseFiles("templates/news.html"))
+		if err = tmpl.Execute(w, news); err != nil {
+			slog.Error("failed to execute template", "newsHandler", err)
+		}
+	} else {
+		// Redirect to the main page if accessed directly
+		log.Println("Redirecting to home page", "newsHandler")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
